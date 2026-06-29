e700a0db8472dedf2bb0b0319ef9bd11ad1d9b3f
fernandodalaaluno-beep <fernandodala.aluno@unipampa.edu.br>
2026-06-29 16:57:08 -0300

    Update exercicio09.py

--- tarefa-02/exercicio09.py
+++ tarefa-02/exercicio09.py
@@ -1,13 +1,13 @@
-forca = 0  
-deformacao = 0  
-falhou = False  
+forca = 0
+deformacao = 0
+falhou = False
 
-while not falhou:  
-    forca += 100  
-    deformacao += forca * 0.0001  
-    print(f"Força: {forca} N | Deformação: {deformacao:.2f} mm")  
-    if deformacao > 5:  
-        falhou = True  
+while not falhou:
+    forca += 100
+    deformacao += forca * 0.0001
+    print(f"Forca: {forca} N | Deformacao: {deformacao:.2f} mm")
+    if deformacao > 5:
+        falhou = True
 
-print(f"\nPeça falhou com força de {forca} N")  
-print(f"Deformação final: {deformacao:.2f} mm")
+print(f"\nPeca falhou com forca de {forca} N")
+print(f"Deformacao final: {deformacao:.2f} mm")
